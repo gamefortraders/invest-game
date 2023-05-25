803725140057ad403f51a10b6efadb9319f4c329
Filip-php <filipp.verh.olantsev@yandex.ru>
2023-05-25 20:58:32 +0500

    deploy

---
+++ js/about.54a89b4c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkinvest_game"]=self["webpackChunkinvest_game"]||[]).push([[443],{1133:function(n,e,t){t.r(e),t.d(e,{default:function(){return o}});var u=t(3396);const r={class:"about"},s=(0,u._)("h1",null,"Обучающая игра про инвестирование",-1),a=[s];function c(n,e){return(0,u.wg)(),(0,u.iD)("div",r,a)}var i=t(89);const f={},l=(0,i.Z)(f,[["render",c]]);var o=l}}]);
+//# sourceMappingURL=about.54a89b4c.js.map
